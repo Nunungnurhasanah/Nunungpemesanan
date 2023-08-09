4560879eff8fde7411df03e9c1ad4ae4dbfedfcb
Nunungnurhasanah <141700307+Nunungnurhasanah@users.noreply.github.com>
2023-08-09 09:53:19 +0700

    Add files via upload

---
+++ pemesanan.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.">
+    <title>pemesanan nasi kotak</title>
+</head>
+<body>
+   <form action="" method="post">
+    <table border="0" align="center">
+        <td><img src="image.png" width="90" height="90"></td>
+      <tr><th colspan="2">FORM PEMESANAN NASI KOTAK</th></tr></br>
+        <tr><td>Cabang :</td></br>
+        <td><select name="Cabang">
+            <option value="-pilih cabang-">-pilih cabang</option>
+            <option value="cempaka">cempaka</option>
+            <option value="jaksel">jaksel</option>
+            </select></td></tr>
+            <tr><td>Nama pelanggan :</td><td><input type="text"name="nama pelanggan"id="nama pelanggan"></td></tr></br>
+            <tr><td>Nomor hp       :</td><td><input type="text"name="nomor hp"id="nomor hp"></td></tr></br>
+            <tr><td>Jumlah kotak   :</td><td><input type="text"name="jumlah kotak"id="jumlah kotak"></td></tr></br>
+            <tr><td>Tagihan awal   :</td><td><input type="text"name="tagihan awal"id="tagihan awal"></td></tr></br>
+            <tr><td>Diskon         :</td><td><input type="text"name="diskon"id="diskon"></td></tr></br>
+            <tr><td>Tagihan akhir  :</td><td><input type="text"name="tagihan akhir"id="tagihan akhir"></td></tr></br>
+            <tr><td><input type="submit"name="pesan"value="pesan"></td><tr>
+</tr>
+</table>
+</form>
+</body>
+
+</html>
